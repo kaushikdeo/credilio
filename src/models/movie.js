a4c4364d02ee6f282785c6950d66ae4c3322e34f
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-31 03:55:47 +0530

    added mongoose and setup mongodb replica set on mongodb atlas

---
+++ src/models/movie.js
@@ -0,0 +1,30 @@
+const {Schema, SchemaTypes, model} = require('mongoose');
+
+const MovieSchema = new Schema({
+    name: {
+        type: String,
+        unique : true,
+        required : true,
+        dropDups: true
+    },
+    pgRating: {
+        type: String,
+    },
+    imdbRating: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    genre: [{
+        type: String
+    }],
+}, {timestamps: true})
+
+module.exports = model('Movie', MovieSchema);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const fetchData = require('./utils/fetchIMDBData');
 
 const {
     PORT = 5000,
     REQUEST_URL = 'https://www.imdb.com/search/title/?groups=top_1000&sort=user_rating,desc&count=10&ref_=adv_nxt',
+    MONGODB_URL = 'mongodb+srv://kaushikmdeo:kaushik123@frontm.zltir.mongodb.net/frontm?retryWrites=true&w=majority',
 } = process.env;
+
+mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 
