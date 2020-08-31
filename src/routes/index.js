5bc23a51ef8bc944c8473ea6191e12829bdce0a5
kaushikmdeo <kaushikmdeo@gmail.com>
2020-08-31 10:39:25 +0530

    added search by name and description

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,14 +9,23 @@ router.get('/movies', async (req, res, next) => {
     const orderBy = Number(req.query.orderBy) === 1 ? 1 : -1;
     const filterString = req.query.filter ? req.query.filter.toString() : "";
     const sortBy = req.query.sortBy ? req.query.sortBy.toString() : 'imdbRating' ;
-    let filterQuery = {}
-    filterQuery = {
-        name: new RegExp(filterString, 'i')
-    }
+    console.log('req.query.filterByDescription', req.query.filterByDescription);
+    const filterByDescription = Number(req.query.filterByDescription) === 0 ? false : true;
+    console.log('filterByDescription', filterByDescription);
+    let filterQuery = {
+        name: new RegExp(filterString, 'i'),
+    };
+    if (filterByDescription) {
+        console.log('I am in');
+        filterQuery = {
+            $or: [{ description: new RegExp(filterString, 'i') }, { name: new RegExp(filterString, 'i'), }]
+        };
+    };
     const sortQuery = {
         [sortBy]: orderBy,
     };
     const moviesLength = await Movie.countDocuments(filterQuery);
+    console.log('filterQuery', filterQuery);
     const allMovies = await Movie.find(filterQuery).limit(limit).skip(startIndex).sort(sortQuery);
     res.send({
         allMovies,
